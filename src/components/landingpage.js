9102d189bf5e8f58226b09cdcc4cdfd5378738fa
larry laconi <soverylarry@gmail.com>
2020-09-20 18:50:08 -0400

    updating landingpage.js and app.js and app.css

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
-class Landingpage extends Component {
+
+class Landing extends Component {
     render() {
-        return(
-            <div>
-                
+        return (
+            <div style={{ width: '100%', margin: 'auto' }}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img src="https://pickaface.net/gallery/avatar/unr_goateebald_170216_1834_yvoxe.png" alt="youveGotMail" className="avatar-img" />
+                        <div className="banner-text">
+                            <h1>Older Bolder Coder Growing Older</h1>
+                            <hr />
+                            <p>HTML/CSS | MERN | Disco Dancing | Git | Javascript | React</p>
+
+                            {/* <div className="social-links">
+                                <a href="http://google.com" rel="noopeneer noreferrer" target="_blank">
+                                    <i className="fa fa_linkedin-square" aria-hidden="true" />
+                                </a>
+                                <a href="http://google.com" rel="noopeneer noreferrer" target="_blank">
+                                    <i className="fa fa_github-square" aria-hidden="true" />
+                                </a>
+                                <a href="http://google.com" rel="noopeneer noreferrer" target="_blank">
+                                    <i className="fa fa_free-code-camp-square" aria-hidden="true" />
+                                </a>
+                                <a href="http://google.com" rel="noopeneer noreferrer" target="_blank">
+                                    <i className="fa fa_youtube-square" aria-hidden="true" />
+                                </a>
+
+                            </div> */}
+                        </div>
+
+                    </Cell>
+                </Grid>
             </div>
         )
     }
 }
 
-export default Landingpage;
+export default Landing;
