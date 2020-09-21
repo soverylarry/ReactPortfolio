b9b19c3c5cb0f37615a4ae645bfb789eb1bb070d
larry laconi <soverylarry@gmail.com>
2020-09-20 20:31:20 -0400

    updating projects.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Contact extends Component {
     render() {
         return(
-            <div><h1>Contact</h1></div>
+            <div></div>
         )
     }
 }
