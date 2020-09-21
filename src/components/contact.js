4eec0da23426c83f6d3107efda302577319c9705
larry laconi <soverylarry@gmail.com>
2020-09-20 21:20:47 -0400

    updating contact.js, app.css and index.html

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
     render() {
-        return(
-            <div></div>
+        return (
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Larry Laconi</h2>
+                        <img src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                            alt="avatar"
+                            style={{ height: '250px' }} />
+
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>loren is an ipsum who annoys the shit out of me all the time. RBG is gone and the country is going to shit</p>
+                    </Cell>
+
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-phone-square" aria-hidden="true" />
+                                        203-927-0818</ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-twitter" aria-hidden="true" />
+                                        @soverylarry</ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-envelope" aria-hidden="true" />
+                                        soverylarry@gmail.com</ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+                                        <i className="fa fa-github" aria-hidden="true" />
+                                        https://github.com/soverylarry</ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+
+            </div>
         )
     }
 }
